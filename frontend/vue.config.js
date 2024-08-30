276d506b8b9410cabb0d248bce3c5405e992e633
ant-shen <104231150+ant-shen@users.noreply.github.com>
2024-08-29 18:02:23 -0700

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,28 +1,5 @@
 module.exports = {
-  //for production
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/land-management-app2/' // Path where app is hosted
-    : '/',
-
-    //for local testing
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:5001',
-          changeOrigin: true,
-          secure: false
-        },
-      },
-    },
-    outputDir: 'docs'
-  };
-
-
-//wihtout cors
-/*
-module.exports = {
-  devServer: {
-    proxy: 'http://localhost:5001',
-  },
+    ? '/land-management-app2/'
+    : '/'
 };
-*/
